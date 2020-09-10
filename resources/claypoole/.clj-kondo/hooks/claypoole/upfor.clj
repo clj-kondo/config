6821b3cce1c8bb1d6b13ae09d788c8581778c6fd
Michiel Borkent <michielborkent@gmail.com>
2020-09-10 23:54:28 +0200

    claypoole improvements

--- resources/claypoole/.clj-kondo/hooks/claypoole/upfor.clj
+++ resources/claypoole/.clj-kondo/hooks/claypoole/upfor.clj
@@ -11,7 +11,7 @@
         (throw (ex-info (str (api/sexpr (first (:children node)))
                              " requires vector for its bindings")
                         (meta binding-vec))))
-      (when (zero? (count (api/sexpr binding-vec)))
+      (when (zero? (count bindings))
         (api/reg-finding! (assoc (meta (first (:children node)))
                                  :message (str (api/sexpr (first (:children node)))
                                                " with no bindings")
@@ -20,12 +20,15 @@
                      (zero? (mod (count (api/sexpr binding-vec)) 2)))
         (throw (ex-info "Binding vector requires an even number of forms"
                         (meta binding-vec))))
-      {:node (api/list-node
-              (list*
-               (api/token-node 'let*)
-               (api/vector-node bindings)
-               `(~@(into [] (take-nth 2 bindings))
-                 ~pool
-                 ~@body)))}
+      {:node
+       (api/list-node
+        (list
+         (api/token-node 'for) binding-vec
+         (api/list-node (list*
+                         (api/token-node 'do) ;; body is wrapped in do because
+                                              ;; for can only have one
+                                              ;; expression in body
+                         pool ;; pool is used
+                         body))))}
       (catch Exception e
         (api/reg-finding! (assoc (ex-data e) :message (ex-message e) :type :hook))))))
--- resources/claypoole/src/example.clj
+++ resources/claypoole/src/example.clj
@@ -1,8 +1,32 @@
 (ns example
   (:require [com.climate.claypoole :as cp]))
 
-(def pool (cp/priority-threadpool 5))
+(cp/upfor []) ;; no bindings provided
 
-(def ordered (cp/pfor pool [x (range 10)
-                            y (range 10)]
-               (example/myfn x y)))
+(def pool)
+
+(cp/upfor pool []) ;; cp/upfor with no bindings
+
+(cp/upfor pool [x (range 10)]) ;; unused binding x
+
+(cp/upfor pool [x (range 10)
+                :let [y (inc x)]] ;; let syntax is recognized
+          y)
+
+;; From the README of claypoole:
+
+(defn myfn [_ _])
+
+(def ordered
+  (let [pool (cp/priority-threadpool 5)]
+    (cp/pfor pool [x (range 10)
+                   y (range x)]
+             (myfn x y))))
+
+(def unordered
+  (let [pool ;; pool binding is used
+        (cp/priority-threadpool 5)]
+    (cp/upfor pool  [x (range 10)
+                     :let [ys (range x)] ;; :let syntax is recognized
+                     y ys]
+              (myfn x y))))

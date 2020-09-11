cfb6c751411ffd03987e32dbd561538c344bba65
Michiel Borkent <michielborkent@gmail.com>
2020-09-11 16:34:56 +0200

    Rum: towards multiple bodies

--- resources/rum/.clj-kondo/hooks/rum.clj
+++ resources/rum/.clj-kondo/hooks/rum.clj
@@ -1,6 +1,25 @@
 (ns hooks.rum
   (:require [clj-kondo.hooks-api :as api]))
 
+(defn fn-body? [x]
+  (and (seq? x)
+       (vector? (first x))))
+
+(defn rewrite-body [mixins body defcs?]
+  (if defcs?
+    (let [[binding-vec & body] (:children body)
+          [state-arg & rest-args] (:children binding-vec)
+          ;; the original vector without the state argument
+          fn-args (assoc binding-vec :children rest-args)
+          body (api/list-node
+                (list* (api/token-node 'let*)
+                       (api/vector-node [state-arg (api/token-node nil)])
+                       state-arg
+                       body))]
+      (api/list-node (cons fn-args (conj mixins body))))
+    (let [[binding-vec & body] (:children body)]
+      (api/list-node (cons binding-vec (concat mixins body))))))
+
 (defn rewrite
   ([node] (rewrite node false))
   ([node defcs?]
@@ -11,34 +30,27 @@
          args (if ?docstring
                 (nnext args)
                 (next args))
-         body
+         bodies
          (loop [args* args
-                mixins []]
+                mixins []
+                bodies []]
            (if (seq args*)
-             (let [a (first args*)]
-               (if (vector? (api/sexpr a))
-                 (if defcs?
-                   (let [[state-arg & rest-args] (:children a)
-                         ;; the original vector without the state argument
-                         fn-args (assoc a :children rest-args)
-                         body (rest args*)
-                         body (api/list-node
-                               (list* (api/token-node 'let*)
-                                      (api/vector-node [state-arg (api/token-node nil)])
-                                      state-arg
-                                      body))]
-                     (concat (when ?docstring [?docstring])
-                             [fn-args]
-                             (conj mixins body)))
-                   (concat (when ?docstring [?docstring])
-                           [a]
-                           (concat mixins (rest args*))))
-                 (recur (rest args*)
-                        (conj mixins a))))
+             (let [a (first args*)
+                   a-sexpr (api/sexpr a)]
+               (cond (vector? a-sexpr) ;; a-sexpr is a binding vec and the rest is the body of the function
+                     [(rewrite-body mixins (api/list-node args*) defcs?)]
+                     :else (recur (rest args*)
+                                  (conj mixins a)
+                                  bodies)))
              args))
          new-node (with-meta
-                    (api/list-node (list* (api/token-node 'defn) component-name body))
+                    (api/list-node (list* (api/token-node 'defn)
+                                          component-name
+                                          (if ?docstring
+                                            (cons ?docstring bodies)
+                                            bodies)))
                     (meta node))]
+     (prn (api/sexpr new-node))
      new-node)))
 
 (defn defc [{:keys [:node]}]
